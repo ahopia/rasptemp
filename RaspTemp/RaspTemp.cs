cc508cf4ee3b26a5b6448adecba64e104828bb4e
Arto Hopia <arto.hopia@gmail.com>
2018-02-11 22:13:52 +0200

    Testiä edelleen

--- RaspTemp/RaspTemp.cs
+++ RaspTemp/RaspTemp.cs
@@ -15,7 +15,7 @@ public class RaspTemp
     /// </summary>
     public static void Main()
     {
-        Console.WriteLine("Toimii taas tämäkin versio");
+        Console.WriteLine("Kakkosversio");
     }
 
 }

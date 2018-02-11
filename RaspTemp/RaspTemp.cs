42aea06319b9af2e0a3e4d6a3269df3c8c5171b3
Arto Hopia <arto.hopia@gmail.com>
2018-02-11 21:20:19 +0200

    ensimmäinen commit

---
+++ RaspTemp/RaspTemp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author arto
+/// @version ..2018
+/// <summary>
+/// 
+/// </summary>
+public class RaspTemp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+
+    }
+
+}

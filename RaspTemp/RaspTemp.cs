8210e6a4aea888dc30b9210ab57a43b8d5c22658
Arto Hopia <arto.hopia@gmail.com>
2018-02-22 19:30:17 +0200

    Tiedoston muutosten monitorointia testettu

--- RaspTemp/RaspTemp.cs
+++ RaspTemp/RaspTemp.cs
@@ -22,7 +22,7 @@ public class RaspTemp
         /// <param name="anturipolku">Hakemistopolku, josta löytyy 1-wireanturi hakemistosta /28*</param>
         /// <param name="w1_slave">Luettava tiedosto</param>
         /// <returns>Valitsee hakemistopolun käyttöjärjestelmän mukaan</returns> 
-        int p = (int)Environment.OSVersion.Platform;    
+        int p = (int)Environment.OSVersion.Platform;
         String anturipolku;                             // Hakupolun alustus
         if ((p == 4) || (p == 6) || (p == 128))         // Valinta Windowsin ja Rasbianin välillä automaattisesti
         {
@@ -42,6 +42,7 @@ public class RaspTemp
         List<double> lampotilat = new List<double>();   // Luodaan lista lämpötiloille
         lampotilat.Add(listaanLisays);                  // lisätään lämpötila listaan
 
+        CreateFileWatcher(anturipolku);
         Console.ReadLine();
     }
 
@@ -54,14 +55,14 @@ public class RaspTemp
     /// <returns>Valitsee hakemistopolun käyttöjärjestelmän mukaan</returns> 
     public static double HaeLampo(string anturipolku)
     {
-        double lampoC= 0.0;
+        double lampoC = 0.0;
         DirectoryInfo hakemistoPolku = new DirectoryInfo(anturipolku);
         foreach (var tiedostoPolku in hakemistoPolku.EnumerateDirectories("28*")) // Luetaan jokainen taulukon alkio kaikista /28* alkavista hakemistoista
         {
             var kokoSisalto = tiedostoPolku.GetFiles("w1_slave").FirstOrDefault().OpenText().ReadToEnd();
 
             // Jaetaan lämpötilan sisältävä merkkijono kahteen
-            string[] tiedostonSisalto = kokoSisalto.Split(new string[] { "t=" }, StringSplitOptions.RemoveEmptyEntries); 
+            string[] tiedostonSisalto = kokoSisalto.Split(new string[] { "t=" }, StringSplitOptions.RemoveEmptyEntries);
             string lampoTeksti = tiedostonSisalto[1];       // Poimitaan merkkijonosta jälkimmäinen, eli lämpötilan sisältävä merkkijono
 
             lampoC = double.Parse(lampoTeksti) / 1000;      // Muutetaan lämpötila numeeriseksi ja jaetaan tuhannella
@@ -70,6 +71,42 @@ public class RaspTemp
         }
 
         return lampoC;
+    }
+
+    /// *** https://stackoverflow.com/questions/721714/notification-when-a-file-changes
+    public static void CreateFileWatcher(string path)
+    {
+        // Create a new FileSystemWatcher and set its properties.
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        watcher.Path = path;
+        /* Watch for changes in LastAccess and LastWrite times, and 
+           the renaming of files or directories. */
+        watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+           | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+        // Only watch text files.
+        watcher.Filter = "testi.txt";
+
+        // Add event handlers.
+        watcher.Changed += new FileSystemEventHandler(OnChanged);
+        watcher.Created += new FileSystemEventHandler(OnChanged);
+        watcher.Deleted += new FileSystemEventHandler(OnChanged);
+        watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+        // Begin watching.
+        watcher.EnableRaisingEvents = true;
+    }
+
+    // Define the event handlers.
+    private static void OnChanged(object source, FileSystemEventArgs e)
+    {
+        // Specify what is done when a file is changed, created, or deleted.
+        Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+    }
+
+    private static void OnRenamed(object source, RenamedEventArgs e)
+    {
+        // Specify what is done when a file is renamed.
+        Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
     }
 
 
